cedf0effd20a921447a7b7f02e03654edf5d4778
justin.leon.GB <justin.leon@grupobabel.com>
2021-05-13 08:59:45 -0600

    Se agrega la clase Conexion, donde se realiza la Accion de la URL a probar

---
+++ src/test/java/Conexión.java
@@ -0,0 +1,5 @@
+public class Conexión {
+    //Accion donde se asigna el sitio a probar
+    final static String BASE_URL = "https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F";
+    final static String CHROME_DRIVER_LOCATION = "chromedriver";
+}
