eee048b1b1f00e36398b374c44639875d7a12f3f
justin.leon.GB <justin.leon@grupobabel.com>
2021-05-13 15:25:52 -0600

    Se agrega la Clase Listeners para realizar los reportes de manera mas clara

---
+++ src/test/java/Listeners.java
@@ -0,0 +1,26 @@
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+public class Listeners extends TestListenerAdapter {
+    public void onTestStart(ITestResult tr){
+        System.out.println("Test started");
+    }
+    public void onTestSuccess(ITestResult tr){
+        System.out.println("Test Passed");
+    }
+    public void onTestFailure(ITestResult tr){
+        System.out.println("Test Failed");
+    }
+    public void onTestSkipped(ITestResult tr){
+        System.out.println("Test Skipped");
+    }
+
+
+
+
+
+
+
+
+
+}
